a15674a5961f7a7006cb36428a27e0458ef82d3c
Tamer Abd Alrazaq <44852913+Tamerabdalrazaq@users.noreply.github.com>
2024-11-15 21:02:43 +0200

    Update hw1secws.c

--- hw1secws.c
+++ hw1secws.c
@@ -13,17 +13,17 @@ static struct nf_hook_ops netfilter_ops_in;
 static struct nf_hook_ops netfilter_ops_out;
 
 static unsigned int module_hook(void *priv, struct sk_buff *skb, const struct nf_hook_state *state) {
+    printk(KERN_INFO "$s\n", state->hook);
     if (state->hook == NF_INET_LOCAL_IN || state->hook == NF_INET_LOCAL_OUT) {
         printk(KERN_INFO "*** Packet Dropped ***\n");
         return NF_DROP;
     }
-
-    // Allow packets that are forwarded through the firewall
-    return NF_ACCEPT;
   
     printk(KERN_INFO "*** Packet Accepted ***\n");
     return NF_ACCEPT;
 }
+
+
 
 static int __init fw_init(void) {
     printk(KERN_INFO "Loading hw1secws module...\n");
@@ -40,9 +40,16 @@ static int __init fw_init(void) {
     netfilter_ops_out.hooknum = NF_INET_POST_ROUTING;
     netfilter_ops_out.priority = NF_IP_PRI_FIRST;
 
+    // Set up the Netfilter hook for outgoing packets
+    netfilter_ops_fwd.hook = module_hook;
+    netfilter_ops_fwd.pf = PF_INET;
+    netfilter_ops_fwd.hooknum = NF_INET_FORWARD;
+    netfilter_ops_fwd.priority = NF_IP_PRI_FIRST;
+
     // Register the hook
     nf_register_net_hook(&init_net, &netfilter_ops_in);
-    nf_register_net_hook(&init_net, &netfilter_ops_out);
+    nf_register_net_hook(&init_net, &netfilter_ops_fwd);
+    nf_register_net_hook(&init_net, &netfilter_ops_fwd);
 
     return 0;
 }
