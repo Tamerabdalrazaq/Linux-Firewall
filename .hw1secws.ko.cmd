d8af15c39f6f32fbede98a003b083f796226e963
vboxuser <vboxuser@WS-Firewall64.myguest.virtualbox.org>
2024-11-15 21:38:04 +0200

    n

---
+++ .hw1secws.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/vboxuser/Desktop/Linux-Firewall/hw1secws.ko := ld -r -m elf_x86_64 -z max-page-size=0x200000 -T ./scripts/module-common.lds --build-id  -o /home/vboxuser/Desktop/Linux-Firewall/hw1secws.ko /home/vboxuser/Desktop/Linux-Firewall/hw1secws.o /home/vboxuser/Desktop/Linux-Firewall/hw1secws.mod.o ;  true
---
+++ .tmp_versions/hw1secws.mod
@@ -0,0 +1,3 @@
+/home/vboxuser/Desktop/Linux-Firewall/hw1secws.ko
+/home/vboxuser/Desktop/Linux-Firewall/hw1secws.o
+
